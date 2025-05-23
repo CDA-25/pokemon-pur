616cab4a3c2759fa29237f4298c16de78a34aff2
Alice <alice.cordier39210@gmail.com>
2025-05-23 16:15:43 +0200

    style accueil

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,10 @@
-(async () => {
-    const res = await fetch("https://pokebuildapi.fr/api/v1/pokemon", { method: "GET" })
-    const date = await res.json()
-    console.log(data)
-})()
+window.addEventListener('load', function () {
+
+    (async () => {
+        const res = await fetch("https://pokebuildapi.fr/api/v1/pokemon", { method: "GET" })
+        const date = await res.json()
+        console.log(data)
+    })()
+
+
+})
