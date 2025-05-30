85b608df83a2d555b447e6161cb39680c82c1602
Alice <alice.cordier39210@gmail.com>
2025-05-30 11:23:23 +0200

    bordure bleue footer padding top

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,28 @@
 window.addEventListener('load', function () {
 
+    class Pokemons {
+        constructor(id, name, image) {
+            this.id = id
+            this.name = name
+            this.image = image
+        }
+    }
+
     (async () => {
         const res = await fetch("https://pokebuildapi.fr/api/v1/pokemon", { method: "GET" })
-        const date = await res.json()
-        console.log(data)
+        const data = await res.json()
+        data.forEach(pokemon => {
+            for (const key in pokemon) {
+                if (Array.isArray(pokemon[key])) {
+                    console.log(`id:: ${pokemon.id}, Pokemon: ${pokemon.name}`);
+                }
+            }
+        });
     })()
 
+
+
+
+    // class pokemon = propriétés de l'objet de l'api etc 
 
 })
