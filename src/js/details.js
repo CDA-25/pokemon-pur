8a80a9bdd2b49eb9549c13aea107029365d4fe68
Alice <alice.cordier39210@gmail.com>
2025-05-30 14:30:37 +0200

    style pour détail avec ajout des classes pour infos

--- src/js/details.js
+++ src/js/details.js
@@ -3,27 +3,27 @@ window.addEventListener('load', async () => {
   const boutonRetour = document.getElementById('btnRetour');
   if (boutonRetour) {
     boutonRetour.addEventListener('click', () => {
-      window.history.back(); // Retourne à la page précédente (repertoire.html)
+      window.history.back()
     });
   }
   const params = new URLSearchParams(window.location.search);
-  const id = params.get('id');
+  const id = params.get('id')
 
   if (!id) {
-    console.error("Aucun ID trouvé dans l'URL");
-    return;
+    console.error("Aucun ID trouvé dans l'URL")
+    return
   }
 
   try {
-    const res = await fetch(`https://pokebuildapi.fr/api/v1/pokemon/${id}`);
-    const pokemon = await res.json();
+    const res = await fetch(`https://pokebuildapi.fr/api/v1/pokemon/${id}`)
+    const pokemon = await res.json()
 
     // Ajoute les infos au DOM :
-    document.querySelector('#pokemonName').textContent = `#${pokemon.id} ${pokemon.name}`;
-    document.querySelector('#pokemonImage').src = pokemon.image;
-    document.querySelector('#pokemonTypes').textContent = pokemon.apiTypes.map(t => t.name).join(', ');
+    document.querySelector('#pokemonName').textContent = `#${pokemon.id} ${pokemon.name}`
+    document.querySelector('#pokemonImage').src = pokemon.image
+    document.querySelector('#pokemonTypes').textContent = pokemon.apiTypes.map(t => t.name).join(', ')
 
   } catch (error) {
-    console.error("Erreur de récupération du Pokémon :", error);
+    console.error("Erreur de récupération du Pokémon :", error)
   }
 });
